bb8e28c172532bde496030b14be0d1e3fb03c88e
IlhamBeni <111227831+IlhamBeni@users.noreply.github.com>
2022-10-21 10:40:14 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/homi.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ABC</title>
+</head>
+<body>
+     <!-- SIDE BAR-->
+
+     <ul class="sidebar">
+        <li class="nav-item">
+            <a href="#A" class="nav-link">
+            <i class="fa fa-file"></i>
+                <span class="title">A</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="#B" class="nav-link">
+            <i class="fa fa-file"></i>
+                <span class="title">B</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="#C" class="nav-link">
+            <i class="fa fa-file"></i>
+                <span class="title">C</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="logout.php" class="nav-link">
+            <i class="fa fa-sign-out"></i>
+                <span class="title">LogOut</span>
+            </a>
+        </li>
+    </ul>
+
+
+    <!-- ISI-->
+        <section id="A" class="a">
+            <h2>A</h2>
+        </section>
+
+        <section id="B" class="b">
+            <h2>B</h2>
+        </section>
+
+        <section id="C" class="c">
+            <h2>C</h2>
+        </section>
+    
+</body>
+</html>
